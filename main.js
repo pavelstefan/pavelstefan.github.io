e092ebaf7438b04cfb5a637a5c7ffa4632669b4f
Pavel Stefan <pavel_stefan_96@yahoo.ro>
2016-05-12 14:10:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-function main(){
-	var minim = document.getElementById("minim").value;
-	var maxim = document.getElementById("maxim").value; 
-	var pas = document.getElementById("password");
-	var nr =  Math.floor((Math.random() * 100)) % (maxim-minim + 1) + minim * 1;
-	pas.value = nr;
+var premii = ["#premiu1", "#premiu2", "#premiu3"];//id-uri premii
+var index = 0;//pentru premii
+var center = "0px";//center position
+var leftMargin = "-266px";//left position outside 
+var rightMargin = "150px";//right position outside
+
+$(document).ready(function () {
+    initPosition();
+    $(".swipe").on("swiperight", function () {
+        swiperight();
+    })
+    $(".swipe").on("swipeleft", function () {
+        swipeleft();
+    })
+});
+
+function initPosition(){    
+    $(premii[1]).css("opacity", "0");
+    $(premii[2]).css("opacity", "0");
+    $(premii[1]).animate({left: rightMargin}, "fast", function(){$(premii[1]).css("opacity", "1");});
+    $(premii[2]).animate({left: rightMargin}, "fast", function(){$(premii[2]).css("opacity", "1");});    
+}
+
+function swiperight(){
+    if(index > 0){
+        $(premii[index]).animate({left: rightMargin});
+        $(premii[index-1]).animate({left: center});
+        index--;
+    }
+}
+
+function swipeleft(){
+    if(index < premii.length-1){
+        $(premii[index]).animate({left: leftMargin});
+        $(premii[index+1]).animate({left: center});
+        index++;
+    }
 }
